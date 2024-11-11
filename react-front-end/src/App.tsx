33d49f5890c5f23ac88ca9e23b68fdeae4b9de9c
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-11-11 10:32:28 -0800

    Geo - 58

---
+++ react-front-end/src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div className="flex justify-center items-center h-screen bg-blue-800">
+        <h1 className="text-4xl font-bold text-black">Hello, Tailwind CSS!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
