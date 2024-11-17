fba16f16434bdad6a78b44614e7fc99f50b17c4e
bernard-ks <158518376+bernard-ks@users.noreply.github.com>
2024-11-16 23:36:03 -0800

    Optimization of link ingestion

--- react-front-end-wip/GEO APP/src/App.tsx
+++ react-front-end-wip/GEO APP/src/App.tsx
@@ -3,8 +3,9 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import { AlignJustify } from "lucide-react";
+import { AlignJustify, Search } from "lucide-react";
 import GeoLogo from "@/assets/hcai-logo.png";
+import CaGovLogo from "@/assets/ca-gov-logo.png";
 import {
   Sheet,
   SheetContent,
@@ -45,55 +46,148 @@ const App: React.FC = () => {
   return (
     <div className="bg-white h-[89vh] flex flex-col overflow-none">
       {/* Navbar */}
-      <nav className="bg-gray-800 fixed top-0 left-0 w-full drop-shadow-lg p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          {/* Logo and Title */}
-          <div className="flex items-center space-x-4">
-            <img src={GeoLogo} alt="Geo Logo" className="h-14" />
-            <div className="text-white text-4xl font-bold"></div>
+      <nav className="fixed top-0 left-0 w-full drop-shadow-lg z-50">
+        <div>
+          {/* Top Navigation */}
+          <div className="bg-white flex justify-between items-center p-5">
+            {/* Logo on the left */}
+            <img
+              src={CaGovLogo}
+              alt="CA Gov Logo"
+              className="h-9 object-contain"
+            />
+
+            {/* Links on the right */}
+            <div className="flex space-x-4 md:space-x-6 text-sm md:text-base">
+              {[
+                {
+                  href: "https://hcai.ca.gov/media-center/",
+                  label: "Newsroom",
+                },
+                {
+                  href: "https://hcai.ca.gov/public-meetings/",
+                  label: "Public Meetings",
+                },
+                { href: "https://hcai.ca.gov/about/", label: "About HCAI" },
+                {
+                  href: "https://hcai.ca.gov/mailing-list/",
+                  label: "Subscribe",
+                },
+              ].map((link) => (
+                <a
+                  key={link.href}
+                  href={link.href}
+                  className="font-semibold text-blue-800"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {link.label}
+                </a>
+              ))}
+            </div>
           </div>
 
-          {/* Navigation Buttons */}
-          <div className="flex space-x-4">
-            <Button
-              className=" bg-gradient-to-r from-blue-500 to-blue-700  hover:from-blue-600 hover:to-blue-800 text-white px-4 py-2 rounded-md"
-              onClick={() => setActivePage("home")}
-            >
-              Home
-            </Button>
-            <Button
-              className=" bg-gradient-to-r from-blue-500 to-blue-700  hover:from-blue-600 hover:to-blue-800 text-white px-4 py-2 rounded-md"
-              onClick={() => setActivePage("hpsa-search")}
-            >
-              HPSA Search
-            </Button>
-            <Button
-              className=" bg-gradient-to-r from-blue-500 to-blue-700  hover:from-blue-600 hover:to-blue-800 text-white px-4 py-2 rounded-md"
-              onClick={() => setActivePage("licensed-healthcare")}
-            >
-              Licensed Healthcare Facilities
-            </Button>
-            <Button
-              className=" bg-gradient-to-r from-blue-500 to-blue-800  hover:from-blue-600 hover:to-blue-800 text-white px-4 py-2 rounded-md"
-              onClick={() => setActivePage("api-reference")}
-            >
-              API Reference
-            </Button>
-            <Sheet>
-              <SheetTrigger className="bg-gradient-to-r from-blue-500 to-blue-800  hover:from-blue-600 hover:to-blue-800h-9 rounded-md px-3">
-                <AlignJustify className="text-white" />
-              </SheetTrigger>
-              <SheetContent className="p-6 bg-gradient-to-br from-gray-100 to-blue-100 rounded-md">
-                <SheetHeader></SheetHeader>
-                <Login />
-              </SheetContent>
-            </Sheet>
+          {/* Search Bar Section */}
+          <div className="bg-gray-50 h-24 shadow-md border-b border-gray-200 flex items-center w-full px-0">
+            <div className="flex justify-between items-center w-full px-4">
+              {/* HCAI Logo */}
+              <img
+                src={GeoLogo}
+                alt="Geo Logo"
+                className="h-12 object-contain"
+              />
+
+              {/* Search Bar */}
+              <div className="flex items-center w-full max-w-md border border-gray-300 rounded-lg px-4 py-3 shadow-sm focus-within:ring-2 focus-within:ring-blue-500 transition duration-200">
+                <input
+                  type="text"
+                  placeholder="Search ..."
+                  className="w-full text-sm md:text-base border-none focus:outline-none"
+                />
+                <Search className="text-gray-500 ml-2" />
+              </div>
+            </div>
+          </div>
+
+          {/* Main Navigation Links */}
+          <div className="bg-blue-800 h-16 flex items-center w-full">
+            <div className="flex justify-evenly text-white text-lg md:text-xl font-bold w-full px-4">
+              <a
+                href="https://hcai.ca.gov/facilities/building-safety/"
+                className="text-center hover:bg-blue-900 transition duration-300 px-4 py-2 flex-grow"
+              >
+                Building Safety & Finance
+              </a>
+              <a
+                href="https://hcai.ca.gov/workforce/financial-assistance/"
+                className="text-center hover:bg-blue-900 transition duration-300 px-4 py-2 flex-grow"
+              >
+                Loan Repayments, Scholarships & Grants
+              </a>
+              <a
+                href="https://hcai.ca.gov/workforce/health-workforce/"
+                className="text-center hover:bg-blue-900 transition duration-300 px-4 py-2 flex-grow"
+              >
+                Workforce Capacity
+              </a>
+              <a
+                href="https://hcai.ca.gov/data/"
+                className="text-center hover:bg-blue-900 transition duration-300 px-4 py-2 flex-grow"
+              >
+                Data & Reports
+              </a>
+              <a
+                href="https://hcai.ca.gov/facility-finder/"
+                className="text-center hover:bg-blue-900 transition duration-300 px-4 py-2 flex-grow"
+              >
+                Facility Finder
+              </a>
+            </div>
+          </div>
+
+          {/* Sub Navigation Section */}
+          <div className="bg-gray-800 p-5 justify-end">
+            <div className="flex space-x-4">
+              <Button
+                className="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white px-4 py-2 rounded-md"
+                onClick={() => setActivePage("home")}
+              >
+                Home
+              </Button>
+              <Button
+                className="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white px-4 py-2 rounded-md"
+                onClick={() => setActivePage("hpsa-search")}
+              >
+                HPSA Search
+              </Button>
+              <Button
+                className="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white px-4 py-2 rounded-md"
+                onClick={() => setActivePage("licensed-healthcare")}
+              >
+                Licensed Healthcare Facilities
+              </Button>
+              <Button
+                className="bg-gradient-to-r from-blue-500 to-blue-800 hover:from-blue-600 hover:to-blue-800 text-white px-4 py-2 rounded-md"
+                onClick={() => setActivePage("api-reference")}
+              >
+                API Reference
+              </Button>
+              <Sheet>
+                <SheetTrigger className="bg-gradient-to-r from-blue-500 to-blue-800 hover:from-blue-600 hover:to-blue-800 h-9 rounded-md px-3">
+                  <AlignJustify className="text-white" />
+                </SheetTrigger>
+                <SheetContent className="p-6 bg-gradient-to-br from-gray-100 to-blue-100 rounded-md">
+                  <SheetHeader></SheetHeader>
+                  <Login />
+                </SheetContent>
+              </Sheet>
+            </div>
           </div>
         </div>
       </nav>
 
       {/* Main Content */}
-      <main className="container mx-auto flex-grow p-6 pt-24">
+      <main className="container mx-auto flex-grow p-6 pt-[240px]">
         {renderContent()}
       </main>
     </div>
