dd640efceda60087a36ef6dec027efc99bdb1930
Ben <eklundbenjaminj@gmail.com>
2025-02-20 18:45:17 -0800

    GEO-78 | Integrated API Key authentication onto Django Ninja API with a development non-secure API key.

--- Geo/api/urls.py
+++ Geo/api/urls.py
@@ -2,15 +2,18 @@ from django.urls import path
 from ninja import NinjaAPI
 from.views import admin_login, admin_logout
 from .routers import router
+from .auth import APIKeyAuth
+from ninja.security import HttpBearer
 
-api = NinjaAPI()
+# defines a NinjaAPI that is locked behind APIKeyAuth security
+api = NinjaAPI(auth=APIKeyAuth())
 
-# Add the router correctly
+# Add the router
 api.add_router("/", router)
 
 # Properly include API URLs
 urlpatterns = [
-    path("", api.urls),  #  This correctly registers all API routes
+    path("", api.urls),  #  This registers all API routes
     path("login/", admin_login, name="admin_login"),  # Login API
     path("logout/", admin_logout, name="admin_logout"),  # Logout API
 ]
